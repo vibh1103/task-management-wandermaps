1f2f6d53cab5bb62efef14366ec52030756bb243
Vibhor Malik <vibhor.malik@deliveryhero.in>
2024-05-04 02:15:08 +0530

    Add absolute path

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './tasks.entity';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task]), DatabaseModule],
